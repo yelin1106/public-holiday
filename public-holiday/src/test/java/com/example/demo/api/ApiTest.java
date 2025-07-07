2099f000f3821c206b1abbfd9fd8462474bf6541
yelin <yelin@yl>
2025-07-07 22:27:09 +0900

    feat : 외부 API 조회 test 추가
    - 외부 API 조회하는 service 기능 test

---
+++ public-holiday/src/test/java/com/example/demo/api/ApiTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.api;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class ApiTest {
+	
+	@Autowired
+	private ApiService apiService;
+	
+	@Test
+	void getCountryCode() {
+		String result = apiService.getCountryCode();
+		Assertions.assertThat(result).isEqualTo("Success");
+	}
+	
+	@Test
+	void getPublicHolidays() {
+		apiService.getCountryCode();
+		
+		String result = apiService.getPublicHolidays();
+		Assertions.assertThat(result).isEqualTo("Success");
+	}
+	
+}
